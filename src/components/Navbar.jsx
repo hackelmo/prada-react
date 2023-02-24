edb3eec73be066003736b0cc85b3a48cdee123da
hackelmo <tokkito88@gmail.com>
2023-02-24 10:30:15 +0900

    [STYLE] Router(Header) with tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HiOutlineShoppingBag, HiOutlineUser, HiUser } from "react-icons/hi";
 
 export default function Navbar() {
   return (
-    <header>
+    <header className="flex justify-between border-b border-gray-300 p-3">
       <Link to="/">
-        <h1>Prada</h1>
+        <img
+          width={120}
+          alt="프라다"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Prada-Logo.svg/1280px-Prada-Logo.svg.png"
+        />
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">AllProducts</Link>
-        <Link to="/carts">MyCart</Link>
+        <Link to="/carts" className="text-2xl">
+          <HiOutlineShoppingBag />
+        </Link>
         <Link to="/products/new">NewProduct</Link>
-        <button>Login</button>
+        <button className="text-2xl">
+          <HiOutlineUser />
+        </button>
       </nav>
     </header>
   );
