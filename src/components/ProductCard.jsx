988d9d2304d14169f3e1a31d00f341b821081634
hackelmo <tokkito88@gmail.com>
2023-02-26 04:17:08 +0900

    [FEAT] Create ProductDetail.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function ProductCard({
+  product,
   product: { id, image, title, category, price },
 }) {
   const navigate = useNavigate();
   return (
     <li
       className=" overflow-hidden cursor-pointer"
-      onClick={() => navigate(`/products/${id}`)}
+      onClick={() => navigate(`/products/${id}`, { state: { product } })}
     >
       <img className="w-full" src={image} alt={id} />
       <div className="mt-2 text-sm flex flex-col gap-1">
